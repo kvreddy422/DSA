23681c1cc5ac196e198baa48490552caf75ea13e
Kalakota <Vaibhav.Kalakota@emc.com>
2017-06-12 17:56:51 +0530

    New code -> insert

---
+++ DSA/LL/insertion/insertTotalNew.c
@@ -0,0 +1,51 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *next;
+};
+
+int main()
+{
+  struct node *head = NULL;
+  // inserts 7 at the begining ; LL = 7->NULL 
+  insertBegin(&head,7);
+  // inserts 5 at the begining ; LL = 5->7->NULL
+  insertBegin(&head,5);
+  // inserts 12 at end; LL = 5->7->12->NULL
+  //insertEnd(&head,12); 
+  //insertEnd(&head,4); // 5->7->12->4->NULL 
+  // insertMiddle(a,b,c,d) a= address of head; b= data; c=postion; d= flag flag (0=postion from head; 1= check for that data and insert after that node) 5->
+  //insertMiddle(&head,21,3,0); // 5->7->12->21->4->NULL
+  //insertMiddle(&head,22,10,0); // Not possible
+  //insertMiddle(&head,23,1,0); // 5->23->7->12->21->4->NULL
+  //insertMiddle(&head,24,23,1); // Placed after node with data 23
+  
+  printList(head);
+  return 0;  
+}
+
+void insertBegin(struct node **head, int data)
+{
+  struct node* new_node= (struct node*)malloc(sizeof(struct node));
+  
+  new_node->data=data;
+  new_node->next=NULL;
+  (*head)=new_node;
+
+}
+
+void printList(struct node *head)
+{
+  if(head!=NULL)
+    printf("%d->",head->data);
+  while(head->next!=NULL)
+  {
+  printf("%d->",head->data);
+  head=head->next;
+  }
+  printf("\n");
+  
+}
