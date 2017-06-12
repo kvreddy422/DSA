717111207cc1598d9bd6a1bbb50aa9aec271f06e
Kalakota <Vaibhav.Kalakota@emc.com>
2017-06-12 23:28:38 +0530

    Insert End not working out.Output same as InsertBegin

--- DSA/LL/insertion/insertTotalNew.c
+++ DSA/LL/insertion/insertTotalNew.c
@@ -17,8 +17,8 @@ int main()
   insertBegin(&head,10);
   insertBegin(&head,50);
   // inserts 12 at end; LL = 5->7->12->NULL
-  //insertEnd(&head,12); 
-  //insertEnd(&head,4); // 5->7->12->4->NULL 
+  insertEnd(&head,12); 
+  insertEnd(&head,4); // 5->7->12->4->NULL 
   // insertMiddle(a,b,c,d) a= address of head; b= data; c=postion; d= flag flag (0=postion from head; 1= check for that data and insert after that node) 5->
   //insertMiddle(&head,21,3,0); // 5->7->12->21->4->NULL
   //insertMiddle(&head,22,10,0); // Not possible
@@ -38,7 +38,17 @@ void insertBegin(struct node **head, int data)
   (*head)=new_node;
 
 }
-
+void insertEnd(struct node **head, int data)
+{
+  struct node* new_node=(struct node*)malloc(sizeof(struct node));
+  
+  new_node->data=data;
+  
+  while(node->next!=NULL)
+    node=node->next;
+  node->next=new_node;
+  new_node->next=NULL;
+}
 void printList(struct node *head)
 {
   while(head->next!=NULL)
