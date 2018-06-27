10a493ca9c2526f7372551a001fd1e6fc03edf01
kvreddy422 <kvreddy422@gmail.com>
2018-06-26 20:34:30 -0700

    Reverse using Recursion ... Error ... printing NULL

---
+++ LL/NK/Prob32_RevInPairs_recursion.c
@@ -0,0 +1,153 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *next;
+};
+
+
+// Reverse the Linked List; ex: 1->2->3->4->X turns out to be 2->1->4->3->X;
+
+struct node *ReversePairWise(struct node *head){
+	struct node *a,*b,*out,*headStore;
+	headStore=head;
+	while (head){
+		/*if(head->next==NULL || head->next->next==NULL ){
+			if(head->next==NULL){
+				return head;			
+			}
+			a=head->next;
+			a->next=head;
+			head->next=NULL;
+			return a;		
+		}*/
+			if(head->next)
+				head=head->next->next;
+			else return head;
+			out=ReversePairWise(head);	
+			a=head;
+			if(a)
+				b=a->next;
+			else 
+				return NULL;
+			if(b==NULL)
+				return a;
+			b->next=a;
+			a->next=out;
+
+					
+	}
+	return b;
+	
+}
+
+void insertBegin(struct node **head, int data)
+{
+  struct node* new_node= (struct node*)malloc(sizeof(struct node));
+  
+  new_node->data=data;
+  new_node->next=(*head);
+  (*head)=new_node;
+
+}
+void insertEnd(struct node *head, int data)
+{
+  struct node* new_node=(struct node*)malloc(sizeof(struct node));
+  
+  new_node->data=data;
+  
+  while(head->next!=NULL)
+    head=head->next;
+  head->next=new_node;
+  new_node->next=NULL;
+}
+
+void insertMiddle(struct node *head,int data,int pos_data,int flag)
+{
+  struct node* new_node=(struct node*) malloc(sizeof(struct node));
+  new_node->data=data;
+  if(flag==0)
+    {
+        int i=0;
+	while(head->next!=NULL && i<=pos_data)
+		{
+			head=head->next;
+			i++;
+		}
+        if(pos_data>i+1)
+		printf("Cannot insert %d at the middle\n",data);
+        else
+	{  
+		new_node->next=head->next;
+       		head->next=new_node;
+	}    		
+    }
+  else
+    {
+	while(head->next!=NULL)
+	{
+		if(head->data==data)
+		{
+			break;
+		}
+		else
+		head=head->next;
+	}
+	if(head->data==data)
+	{
+		new_node->next=head->next;
+		head->next=new_node;
+	}
+	else
+		printf("Node with the given data not found \n");				
+    }		
+}
+void printList(struct node *head)
+{
+  struct node *headStore=head;
+  if(head==NULL){
+	printf("No head present");
+	return 0;
+  }
+				
+  while(head->next!=NULL)
+  {
+  printf("%d->",head->data);
+  head=head->next;
+  }
+  printf("%d",head->data);
+  printf("\n");
+ // head=headStore;	
+}
+
+int main()
+{
+  struct node *head = NULL;
+  insertBegin(&head,7);
+  // inserts 5 at the begining ; LL = 5->7->NULL
+  insertBegin(&head,5);
+  insertBegin(&head,10);
+  // inserts 12 at end; LL = 5->7->12->NULL
+  insertEnd(head,22); 
+  insertEnd(head,33);
+  //insertEnd(head,12);
+  //insertEnd(head,44);
+  //insertEnd(head,55);	 
+  //insertEnd(head,4); // 5->7->12->4->NULL 
+  // insertMiddle(a,b,c,d) a= address of head; b= data; c=postion; d= flag flag (0=postion from head; 1= check for that data and insert after that node) 5->
+  //insertMiddle(head,21,1,0); // 5->7->12->21->4->NULL
+ // insertMiddle(head,22,10,0); // Not possible
+  //insertMiddle(&head,23,1,0); // 5->23->7->12->21->4->NULL
+  //insertMiddle(&head,24,21,1); // Placed after node with data 23
+  
+  printf("\n List before Revesing ");	
+  printList(head);
+  head=ReversePairWise(head);  	
+  //ReversePairWise(head);
+  printf("\n List after Revesing ");	
+  printList(head);		
+  return 0;  
+}
+
