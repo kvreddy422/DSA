71e778b6fb23923e3d4295eed57f95f8e72db206
kvreddy422 <kvreddy422@gmail.com>
2018-07-14 15:13:04 -0700

    Main code at the bottom

--- Algorithms/Sorting/insertionSort.c
+++ Algorithms/Sorting/insertionSort.c
@@ -7,29 +7,6 @@ struct node
   struct node *next;
 };
 
-int main()
-{
-  struct node *head = NULL,*headNew;
-  // inserts 7 at the begining ; LL = 7->NULL 
-  insertBegin(&head,2);
-  // inserts 5 at the begining ; LL = 5->7->NULL
-  insertBegin(&head,3);
-  //insertBegin(&head,1);
-  // inserts 12 at end; LL = 5->7->12->NULL
-  insertEnd(head,4); 
-  //insertEnd(head,5); // 5->7->12->4->NULL 
-  // insertMiddle(a,b,c,d) a= address of head; b= data; c=postion; d= flag flag (0=postion from head; 1= check for that data and insert after that node) 5->
-  //insertMiddle(head,21,1,0); // 5->7->12->21->4->NULL
- // insertMiddle(head,22,10,0); // Not possible
-  //insertMiddle(&head,23,1,0); // 5->23->7->12->21->4->NULL
-  //insertMiddle(&head,24,21,1); // Placed after node with data 23
-  
-  printList(head);
-  headNew=insertionSort(head);
-  printf("\nHeadNew:%d\n",headNew->data);	
-  printList(headNew);
-  return 0;  
-}
 /*This code never works. The reason is that the 1st node... the intial node if replaced ex: 3->2->4 will not give 2->3->4; it is giving 3->4 as the output. The reason being you are using temp variable intially is 3. Now it can't change to 2. It will remain 3
 */
 struct node *insertionSort(struct node *head)
@@ -146,4 +123,27 @@ void printList(struct node *head)
   printf("\n");
 }
 
+int main()
+{
+  struct node *head = NULL,*headNew;
+  // inserts 7 at the begining ; LL = 7->NULL 
+  insertBegin(&head,2);
+  // inserts 5 at the begining ; LL = 5->7->NULL
+  insertBegin(&head,3);
+  //insertBegin(&head,1);
+  // inserts 12 at end; LL = 5->7->12->NULL
+  insertEnd(head,4); 
+  //insertEnd(head,5); // 5->7->12->4->NULL 
+  // insertMiddle(a,b,c,d) a= address of head; b= data; c=postion; d= flag flag (0=postion from head; 1= check for that data and insert after that node) 5->
+  //insertMiddle(head,21,1,0); // 5->7->12->21->4->NULL
+ // insertMiddle(head,22,10,0); // Not possible
+  //insertMiddle(&head,23,1,0); // 5->23->7->12->21->4->NULL
+  //insertMiddle(&head,24,21,1); // Placed after node with data 23
+  
+  printList(head);
+  headNew=insertionSort(head);
+  printf("\nHeadNew:%d\n",headNew->data);	
+  printList(headNew);
+  return 0;  
+}
 
