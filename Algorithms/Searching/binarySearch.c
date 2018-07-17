52f3ef448873bb4868198c1414db597a3fb4000f
kvreddy422 <kvreddy422@gmail.com>
2018-07-17 10:00:19 -0700

    Reversal works

--- Algorithms/Searching/binarySearch.c
+++ Algorithms/Searching/binarySearch.c
@@ -9,7 +9,7 @@ int binarySearch(int arr[], int l, int r, int x)
 		int mid=(l+r)/2;
 		if(arr[mid]==x)
 			return mid;
-		if(arr[mid]>x)
+		if(arr[mid]<x)
 			result = binarySearch(arr,mid+1,r,x);
 		else
 			result = binarySearch(arr,l,mid-1,x);   
