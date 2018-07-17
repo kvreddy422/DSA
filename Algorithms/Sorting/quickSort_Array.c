853629bbeb9f5c5533e23ed06ceaab8cd8b87146
kvreddy422 <kvreddy422@gmail.com>
2018-07-16 20:50:45 -0700

    Partition code not correct -> Pivot doesn't come inbetween

---
+++ Algorithms/Sorting/quickSort_Array.c
@@ -0,0 +1,50 @@
+/* C implementation QuickSort */
+#include<stdio.h>
+ 
+// A utility function to swap two elements
+/*void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+*/
+ 
+
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	for (int i=0;i<(high-low);i++){
+		if(arr[i]>high){
+			swap(arr[i],arr[i+1]);		
+		}	
+	}
+}
+ 
+
+void quickSort(int arr[], int low, int high)
+{
+	if(low<high){
+		int pi=partition(arr,low,high);	
+	}    
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5, 12, 4, 6, 2, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+    printf("Sorted array: n");
+    printArray(arr, n);
+    return 0;
+}
