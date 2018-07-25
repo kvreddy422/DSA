a3b6912899ac8574b73727613ba936bed4a55321
kvreddy422 <kvreddy422@gmail.com>
2018-07-24 23:00:44 -0700

    Almost good; not printing the right side nodes inbetween root and leaves

--- Trees/BinaryTree/boundaryTraversal.c
+++ Trees/BinaryTree/boundaryTraversal.c
@@ -18,31 +18,42 @@ struct tree* newNode(int data){
 }
 
 void printBoundaryNodesLeft(struct tree *T){
-	if(T){
-		printf("%d",T->data);
+	if(T && T->left!=NULL && T->right!=NULL){
+		printf("%d,",T->data);
 		printBoundaryNodesLeft(T->left);	
 	}
 }
 
-void printBoundaryNodesRight(struct tree *T){
+void printChildNodes(struct tree *T){
 	if(T){
-		printf("%d",T->data);
-		printBoundaryNodesRight(T->right);		
+		if(T->left==NULL && T->right==NULL)
+			printf("%d,",T->data);
+		printChildNodes(T->left);
+		printChildNodes(T->right);
+	}
+}
+
+void printBoundaryNodesRight(struct tree *T){
+	if(T && T->left!=NULL && T->right!=NULL){
+		printBoundaryNodesRight(T->right);	
+		printf("%d,",T->data);	
 	}
 }
 
 int main()
 {
-     struct tree *root  = newNode(1);
-     root->left = newNode(2);
-     root->right = newNode(3);
+     struct tree *root  = newNode(20);
+     root->left = newNode(8);
+     root->right = newNode(22);
      root->left->left = newNode(4);
-     root->left->right = newNode(5); 
-     root->right->left = newNode(6);
-     root->right->right = newNode(7); 
+     root->left->right = newNode(12);
+     root->left->right->left = newNode(10);
+     root->left->right->right = newNode(14);	 
+     root->right->right = newNode(25); 
  
      printf("\nPreorder traversal of binary tree is \n");
      printBoundaryNodesLeft(root);
+     printChildNodes(root);	
      if(root->right)	
      printBoundaryNodesRight(root->right);
      getchar();
